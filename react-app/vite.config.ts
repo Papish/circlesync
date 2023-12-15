5799480befb2745c24d6f95eebe8fe484a9c00fd
Papish Limbu <papish.limbu@omnicommediagroup.com>
2023-12-15 10:46:40 +0545

    config

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -7,9 +7,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "https://papish.github.io/circlesync/",
-  },
+  base: "/circlesync",
   test: {
     globals: true,
     environment: "jsdom",
